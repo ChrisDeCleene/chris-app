89431147121088fe0f88143a7e40970e8934d048
Chris DeCleene <chris_decleene@ymail.com>
2021-11-17 11:08:55 -0700

    Change json data render in return statement.

--- src/Components/util/ESPN.js
+++ src/Components/util/ESPN.js
@@ -1,6 +1,5 @@
 const fetch = require('node-fetch');
 const username ='teamrebecca';
-const userId = '';
 
 const ESPN = {
   async getUserId() {
@@ -8,7 +7,7 @@ const ESPN = {
       const response = await fetch(`https://api.sleeper.app/v1/user/${username}`);
       if (response.ok) {
         const jsonResponse = await response.json();
-        console.log(jsonResponse.user_id);
+        return jsonResponse.user_id;
       }
       throw new Error('Request failed!');
     } catch (error) {
